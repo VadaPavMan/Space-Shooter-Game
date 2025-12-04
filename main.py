8a6277e7584fc154e593821bfc496943c0d0e8b9
Harsh Rajbhar <harshrajbhar071@gmail.com>
2025-12-04 18:14:38 +0530

    changes in main and shoot file

--- main.py
+++ main.py
@@ -19,16 +19,27 @@ class Gameview(arcade.Window):
         self.update_background_size(width, height)
         
         self.set_minimum_size(WINDOW_WIDTH, WINDOW_HEIGHT)
-        self.set_mouse_visible(True)
+        self.set_mouse_visible(False)
         
         self.player = __hero__.Player(width, height)
+        
+        # Mouse Cursor
+        self.mouse_circle_center_x = width // 2
+        self.mouse_circle_center_y = height // 2
+        self.mouse_circle_radius = 20
+        self.mouse_circle_color = arcade.color.YELLOW
         
         self.enemies = []
         self.particles = []
         self.bullets = []
         
-        for _ in range(5):
+        self.spawn_timer = 0
+        self.max_enemies = 5
+        self.spawn_interval = 1.0 
+        
+        for _ in range(self.max_enemies):
             self.enemies.append(enemies.Enemies(width, height))
+            
         for _ in range(5):
             self.particles.append(particles.Particle(width, height))
             
@@ -51,6 +62,7 @@ class Gameview(arcade.Window):
         for enemy in self.enemies:
             enemy.draw()
             
+        self.mouse_circle = arcade.draw_circle_outline(self.mouse_circle_center_x, self.mouse_circle_center_y, self.mouse_circle_radius, self.mouse_circle_color, 3)
         self.player.draw()
         
     def on_resize(self, width, height):
@@ -59,14 +71,56 @@ class Gameview(arcade.Window):
             
     def on_update(self, delta_time):
         self.player.update(self.width, self.height, delta_time)
-        pos_x, pos_y = self.player.get_position()
         
+        if self.player.shoot():
+                bullet_x, bullet_y = self.player.get_position()
+                angle = self.player.get_angle()
+                new_bullet = shoot.Bullet(angle, bullet_x, bullet_y)
+                self.bullets.append(new_bullet)
+        
+        pos_x, pos_y = self.player.get_position()
         for enemy in self.enemies:
-            enemy.update(delta_time, pos_x, pos_y, self.width, self.height)
-        # self.enemies.update(delta_time, pos_x, pos_y, self.width, self.height)
+            enemy.update(delta_time, pos_x, pos_y, self.width, self.height, self.enemies)
         
         for bullet in self.bullets:
             bullet.update()
+        
+        bullets_to_remove = []
+        enemies_to_remove = []
+
+        for bullet in self.bullets:
+            bullet_sprite = getattr(bullet, 'bullet', getattr(bullet, 'sprite', bullet))
+            
+            hit_enemy = None
+            for enemy in self.enemies:
+                if enemy in enemies_to_remove:
+                    continue
+                
+                if arcade.check_for_collision(bullet_sprite, enemy.enemy):
+                    hit_enemy = enemy
+                    break 
+            
+            if hit_enemy:
+                bullets_to_remove.append(bullet)
+                
+                is_dead = hit_enemy.take_damage()
+                
+                if is_dead:
+                    enemies_to_remove.append(hit_enemy)
+
+        for cal in bullets_to_remove:
+            if cal in self.bullets:
+                self.bullets.remove(cal)
+        
+        for enm in enemies_to_remove:
+            if enm in self.enemies:
+                self.enemies.remove(enm)
+
+        if len(self.enemies) < self.max_enemies:
+            self.spawn_timer += delta_time
+            if self.spawn_timer >= self.spawn_interval:
+                self.enemies.append(enemies.Enemies(self.width, self.height))
+                self.spawn_timer = 0 
             
         for i in range(len(self.bullets) - 1, -1, -1):
             if self.bullets[i].off_screen(self.width, self.height):
@@ -84,13 +138,16 @@ class Gameview(arcade.Window):
     def on_mouse_motion(self, x, y, dx, dy):
         self.player.on_mouse_motion(x, y, dx, dy)
         
+        self.mouse_circle_center_x = x
+        self.mouse_circle_center_y = y
+        
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            if self.player.shoot():
-                bullet_x, bullet_y = self.player.get_position()
-                angle = self.player.get_angle()
-                new_bullet = shoot.Bullet(angle, bullet_x, bullet_y)
-                self.bullets.append(new_bullet)
+            self.player.on_mouse_press()
+            
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.player.on_mouse_release()
 
 
 if __name__ == "__main__":
--- shoot.py
+++ shoot.py
@@ -2,22 +2,24 @@ import arcade
 import math
 import random
 
-class Bullet:
+class Bullet(arcade.Sprite):
     def __init__(self, angle, x, y):
-        self.x = x
-        self.y = y
-        self.angle = angle
+        self.radius = 0.4
+        self.bullet = arcade.Sprite("assets/bullets/06.png", self.radius)
+        self.bullet.center_x = x
+        self.bullet.center_y = y
+        self.bullet.angle = angle
         
         self.speed = 10
-        self.radius = 3
+        
         
     def update(self):
-        angle_rad = math.radians(self.angle)
-        self.x += math.sin(angle_rad) * self.speed
-        self.y += math.cos(angle_rad) * self.speed
+        angle_rad = math.radians(self.bullet.angle)
+        self.bullet.center_x += math.sin(angle_rad) * self.speed
+        self.bullet.center_y += math.cos(angle_rad) * self.speed
     
     def draw(self):
-        arcade.draw_circle_filled(self.x, self.y, self.radius, arcade.color.YELLOW)
+        arcade.draw_sprite(self.bullet)
     
     def off_screen(self, width, height):
-        return (self.x < -10 or self.x > width + 10 or self.y < -10 or self.y > height + 10)
+        return (self.bullet.center_x < -10 or self.bullet.center_x > width + 10 or self.bullet.center_y < -10 or self.bullet.center_y > height + 10)
