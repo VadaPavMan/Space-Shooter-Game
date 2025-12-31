8c6d38f953fba445b543d6989b26673a883af75d
Harsh Rajbhar <harshrajbhar071@gmail.com>
2025-12-31 21:24:26 +0530

    Fixed Credit Tab

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import arcade
 from resources import resource_path, load_texture_cached, load_sound_cached
 import time
 import main
+import webbrowser
 
 
 class Button:
@@ -67,7 +68,7 @@ class StartMenuView(arcade.View):
         self.mouse_y = 0
         self.background = None
         bg_start_music = load_sound_cached("assets/sound/start_menu.mp3")
-        self.background_music = arcade.play_sound(bg_start_music)
+        self.background_music = arcade.play_sound(bg_start_music, loop= True)
         
     def on_show_view(self):
         if self.background is None:
@@ -92,7 +93,7 @@ class StartMenuView(arcade.View):
             Button(center_x, start_y - button_spacing, button_width, button_height, 
                    "OPTIONS", arcade.color.DARK_BLUE, arcade.color.BLUE),
             Button(center_x, start_y - button_spacing * 2, button_width, button_height, 
-                   "CREDITS", arcade.color.DARK_BLUE, arcade.color.BLUE),
+                   "CREDITS", arcade.color.SAE, arcade.color.AMBER),
             Button(center_x, start_y - button_spacing * 3, button_width, button_height, 
                    "EXIT", arcade.color.DARK_RED, arcade.color.RED)
         ]
@@ -132,6 +133,7 @@ class StartMenuView(arcade.View):
             
         elif self.buttons[2].is_hovered(x, y):
             print("CREDITS clicked")
+            arcade.stop_sound(self.background_music)
             credits_view = CreditTab(self.game_view)
             self.window.show_view(credits_view)
             
@@ -148,7 +150,7 @@ class PauseMenuView(arcade.View):
         self.mouse_x = 0
         self.mouse_y = 0
         bg_start_music = load_sound_cached("assets/sound/pause_menu.mp3")
-        self.background_music = arcade.play_sound(bg_start_music)
+        self.background_music = arcade.play_sound(bg_start_music, loop= True)
         
     def on_show_view(self):
         self.window.set_mouse_visible(True)
@@ -279,40 +281,181 @@ class CountdownView(arcade.View):
             self.window.show_view(self.game_view)
 
 class CreditTab(arcade.View):
-    def __init__(self, game_view, dead= False):
+    def __init__(self, game_view):
         super().__init__()
         config.config()
         self.game_view = game_view
         self.mouse_x = 0
         self.mouse_y = 0
-        self.text = "Background Music From: oblidivm\nLink: https://opengameart.org/users/oblidivm"
+        self.background = None
         bg_start_music = load_sound_cached("assets/sound/pause_menu.mp3")
-        self.background_music = arcade.play_sound(bg_start_music)
+        self.background_music = arcade.play_sound(bg_start_music, loop= True)
+        self.back_button = None
+        
+        self.link_url = "https://opengameart.org/users/oblidivm"
+        self.link_rect = None
+        self.link_hovered = False
         
     def on_show_view(self):
+        if self.background is None:
+            self.background = arcade.Sprite(load_texture_cached("assets/startscreen.png"))
+        
+        self.background.center_x = self.width // 2
+        self.background.center_y = self.height // 2
+        self.background.alpha = 255
         self.window.set_mouse_visible(True)
-        self.create_buttons()
+        center_x = self.window.width // 2
+        button_y = 100
+        self.back_button = Button(
+            center_x, button_y, 250, 60,
+            "BACK TO MENU", arcade.color.AMBER, arcade.color.SAE
+        )
         
     def on_draw(self):
+        self.clear()
+        arcade.draw_sprite(self.background)
         arcade.draw_lrbt_rectangle_filled(
             0, self.window.width,
             0, self.window.height,
-            (0, 0, 0, 200)
+            (0, 0, 0, 60)
         )
         
         arcade.draw_text(
-            self.text,
+            "CREDITS",
             self.window.width // 2,
-            self.window.height // 2,
+            self.window.height - 100,
             arcade.color.YELLOW,
             50,
             anchor_x="center",
             anchor_y="center",
             bold=True
-        ) 
+        )
+        
+        # Credits
+        credits_text = [
+            "Background Music Credit:",
+            "oblidivm",
+            "",
+            "Background Music Link:",
+            "https://opengameart.org/users/oblidivm",
+            "",
+            "Game developed with Arcade Library",
+            "",
+            "Press ESC or click BACK TO MENU to return"
+        ]
+        
+        start_y = self.window.height - 200
+        line_spacing = 40
+        
+        for i, line in enumerate(credits_text):
+            y_pos = start_y - (i * line_spacing)
+            
+            if "https://" in line:
+                color = arcade.color.ORANGE if self.link_hovered else arcade.color.YELLOW
+                size = 20
+                
+                arcade.draw_text(
+                    line,
+                    self.window.width // 2,
+                    y_pos,
+                    color,
+                    size,
+                    anchor_x="center",
+                    anchor_y="center",
+                    bold=True
+                )
+                
+                text_width = len(line) * 10
+                text_height = size
+                
+                self.link_rect = (
+                    self.window.width // 2 - text_width // 2,
+                    y_pos - text_height // 2,
+                    text_width,  
+                    text_height  
+                )
+                
+                
+                arcade.draw_text(
+                    "(click to open)",
+                    self.window.width // 2,
+                    y_pos - 25,
+                    arcade.color.LIGHT_GRAY,
+                    12,
+                    anchor_x="center",
+                    anchor_y="center",
+                    italic=True
+                )
+                
+            elif line == "oblidivm":
+                color = arcade.color.YELLOW
+                size = 20
+                arcade.draw_text(
+                    line,
+                    self.window.width // 2,
+                    y_pos,
+                    color,
+                    size,
+                    anchor_x="center",
+                    anchor_y="center",
+                    bold=True
+                )
+            elif line in ["Background Music:", "Assets Link:", "Game developed with Arcade Library"]:
+                color = arcade.color.WHITE
+                size = 24
+                arcade.draw_text(
+                    line,
+                    self.window.width // 2,
+                    y_pos,
+                    color,
+                    size,
+                    anchor_x="center",
+                    anchor_y="center"
+                )
+            else:
+                color = arcade.color.LIGHT_GRAY
+                size = 18
+                arcade.draw_text(
+                    line,
+                    self.window.width // 2,
+                    y_pos,
+                    color,
+                    size,
+                    anchor_x="center",
+                    anchor_y="center"
+                )
+        
+        if self.back_button:
+            self.back_button.draw()
+    
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_x = x
+        self.mouse_y = y
+        
+        if self.back_button:
+            self.back_button.update_hover(x, y)
+        
+        if self.link_rect:
+            left, bottom, width, height = self.link_rect
+            if (left <= x <= left + width and 
+                bottom <= y <= bottom + height):
+                self.link_hovered = True
+            else:
+                self.link_hovered = False
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.link_rect:
+            left, bottom, width, height = self.link_rect
+            if (left <= x <= left + width and 
+                bottom <= y <= bottom + height):
+                webbrowser.open(self.link_url)
+                return
+        
+        if self.back_button and self.back_button.is_hovered(x, y):
+            arcade.stop_sound(self.background_music)
+            self.window.show_view(StartMenuView(self.game_view))
         
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
             arcade.stop_sound(self.background_music)
-            self.game_view.reset_game()
             self.window.show_view(StartMenuView(self.game_view))
